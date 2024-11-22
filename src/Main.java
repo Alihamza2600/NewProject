bf79f0d5a3da934c30413e890ed573d6e4d7e3da
Alihamza2600 <s235479@dtu.dk>
2024-11-22 03:34:31 +0100

    Name changed to age
    Name changed to age

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter your name");
+        System.out.println("Enter your age");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println("Your name is " + name);
+        int age = scanner.nextInt();
+        System.out.println("Your age is " + age);
     }
 }
